1822f674837785f21dd5e0c781015760132b91b1
habeeb <habeebalao101@gmail.com>
2022-01-25 23:51:53 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -1,9 +1,30 @@
 package ie.tudublin;
 
-public class Cat extends Animal
-{
+public class Cat {
+
+    private Integer numLives = 9;
+    private String name;
+
     public Cat(String name)
     {
-        super(name);
+        this.name = name;
     }
+
+    public void kill()
+    {
+        this.numLives = this.numLives -1;
+
+        if (this.numLives > 0) {
+            System.out.println("Ouch!");
+        }
+        else{
+            System.out.println("Dead");
+        }
+    }
+
+    public String toString()
+    {
+        return name;
+    }
+    
 }
