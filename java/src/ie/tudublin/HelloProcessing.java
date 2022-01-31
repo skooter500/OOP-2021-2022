277bff06d3740313cf93c7b9cfb93ff0af144303
Xelqi <milek1093@gmail.com>
2022-01-31 16:42:09 +0000

    Crazy eye

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,57 +11,53 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		//colorMode(HSB); Hue saturation Brightness
+		colorMode(RGB);
 		background(0);
-
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
-		smooth();
-		
 	}
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
-	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
-
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
+		stroke(255); 
+		background	(255, 0, 0); //greyscale
+		//coordinates x1 y1 x2 y2
+		//line(10, 10, 200, 200);
+		// cx, xy ,r
+		fill(255, 255, 0);
+		circle(250, 250, 400);
 		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
-		}
+		// cx, cy, w h
+		//ellipse(50, 200, 90, 200);
 
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
-		}
+		//stroke(0);
+
+
+		//rectMode(CENTER);
+		//rect(300, 20, 100, 20);
+		// tlx, tly, w ,h
+		//rectMode(CORNER);
+		//rect(300, 20, 100, 20);
+
+		// to plot a single pixle
+		//point(20, 300); 
+
+		// x1, y1, x2, y2, x3, y3
+		fill(0, 255, 255);
+		stroke(0);
+		triangle(250, 20, 50, 400, 450, 400);
+		
+		fill(0, 0, 0);
+		stroke(255);
+		ellipse(250, 250, 210, 90);
+
+		fill(255, 255, 255);
+		stroke(0, 0, 0);
+		ellipse(250, 250, 60, 90);
+
+		stroke(0);
+		fill(255, 0, 0);
+		ellipse(250, 250, 30, 90);
+		
+		
 	}
 }
