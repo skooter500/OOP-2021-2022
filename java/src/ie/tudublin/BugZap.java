f088a11946c27d13dd28a98f508eda00ba69aa48
Bryan Duggan <skooter500@gmail.com>
2022-02-03 09:41:29 +0000

    bug moving

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -6,6 +6,8 @@ public class BugZap extends PApplet
 {
 	float playerX, playerY, playerWidth;
 
+	float bugX, bugY, bugWidth;
+
 	public void settings()
 	{
 		size(500, 500);
@@ -19,7 +21,23 @@ public class BugZap extends PApplet
 		playerX = width / 2;
 		playerY = height - 50;
 		playerWidth = 50;
+	
+		resetBug();
 		
+	}
+
+	private void resetBug() {
+		bugX = random(bugWidth / 2, width - (bugWidth / 2));
+		bugY = 50;
+		bugWidth = 50;
+	}
+
+	void drawBug(float x, float y, float w)
+	{
+		float halfW = w / 2;
+		stroke(255);
+		noFill();
+		triangle(x - halfW, y + halfW, x, y - halfW, x + halfW, y + halfW);
 	}
 
 	void drawPlayer(float x, float y, float w)
@@ -31,10 +49,34 @@ public class BugZap extends PApplet
 		line(x, y - 10, x, y - 20);
 	}
 
+	float playerSpeed = 5;
+
+	public void keyPressed()
+	{
+		if (keyCode == LEFT)
+		{
+			playerX -=playerSpeed;
+		}
+		if (keyCode == RIGHT)
+		{
+			playerX +=playerSpeed;
+		}
+		
+	}
+
+	void moveBug()
+	{
+		bugY ++;
+		bugX += random(-20, 20);
+	}
+
 	
 	public void draw()
 	{	background(0);
 		strokeWeight(2);
 		drawPlayer(playerX, playerY, playerWidth);
+		drawBug(bugX, bugY, bugWidth);
+		if (frameCount % 20 == 0)
+			moveBug();
 	}
 }
