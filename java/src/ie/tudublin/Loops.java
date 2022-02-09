c1dcd3b3d001efcb20626a40a7855df7e74ce3f0
Bryan Duggan <skooter500@gmail.com>
2022-02-09 15:26:41 +0000

    lab

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -5,6 +5,9 @@ import processing.core.PApplet;
 public class Loops extends PApplet
 {
 
+
+	int mode = 0;
+
 	public void settings()
 	{
 		size(500, 500);
@@ -15,8 +18,25 @@ public class Loops extends PApplet
 		
 	}
 
+	public void keyPressed()
+	{
+		if (key >= '0' && key <='9')
+		{
+			mode = key - '0';
+		}
+		println(mode);
+	}
+
 	
 	public void draw()
-	{	
+	{
+		
+		switch(mode)	
+		{
+			case 0:
+				break;
+			case 1:
+				break;
+		}
 	}
 }
