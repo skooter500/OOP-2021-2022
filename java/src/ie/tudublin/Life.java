772b7e7a2fcfc11288302b620ad2aa76ac11efc0
Bryan Duggan <skooter500@gmail.com>
2022-03-07 11:46:39 +0000

    bla

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -3,9 +3,14 @@ package ie.tudublin;
 import processing.core.PApplet;
 
 public class Life extends PApplet {
+	
+	LifeBoard board;
+
 
 	public void settings() {
 		size(500, 500);
+		board = new LifeBoard(100, this);
+		board.randomise();
 	}
 
 	public void setup() {
@@ -15,5 +20,6 @@ public class Life extends PApplet {
 
 	
 	public void draw() {
+		board.render();
 	}
 }
---
+++ java/src/ie/tudublin/LifeBoard.java
@@ -0,0 +1,34 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class LifeBoard {
+    boolean[][] board;
+    int size;
+    float cellSize;
+    PApplet pa;
+
+    public LifeBoard(int size, PApplet pa)
+    {
+        board = new boolean[size][size];
+        this.size = size;
+        this.pa = pa;
+        cellSize = pa.width / (float) size;
+    }
+
+    public void randomise()
+    {
+        for(int row = 0 ; row < size ; row ++)
+        {
+            for(int col = 0 ; col < size ; col ++)
+            {
+                board[row][col] = pa.random(1.0f) > 0.5f;
+            }
+        }
+    }
+
+    public void render()
+    {
+        
+    }
+}
