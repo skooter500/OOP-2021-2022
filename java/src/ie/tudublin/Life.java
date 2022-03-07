70470c93f36683d0ba9aa2d5305c41af5aa45ff8
Bryan Duggan <skooter500@gmail.com>
2022-03-07 10:48:23 +0000

    life starter

---
+++ java/src/ie/tudublin/Life.java
@@ -0,0 +1,19 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Life extends PApplet {
+
+	public void settings() {
+		size(500, 500);
+	}
+
+	public void setup() {
+		colorMode(HSB);
+
+	}
+
+	
+	public void draw() {
+	}
+}
