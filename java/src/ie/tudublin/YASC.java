b8bcd1eaed708612e0fac9291c2128a930ae12a7
Bryan Duggan <skooter500@gmail.com>
2022-04-04 11:32:04 +0100

    bla

--- java/src/ie/tudublin/YASC.java
+++ java/src/ie/tudublin/YASC.java
@@ -2,17 +2,33 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class YASC extends PApplet{
+public class YASC extends PApplet
+{
+    float x, y;
+    float w;
+
+    void drawPlayer(float x, float y, float w)
+    {
+        
+    }
+
 
     public void setup()
     {
+        x = width / 2;
+        y = height / 2;
+        w = 100;
     }
 
     public void settings()
     {
+        size(500,500);
     }
 
     public void draw()
     {
+        background(0);
+
+        drawPlayer(x, y, w);
     }    
 }
